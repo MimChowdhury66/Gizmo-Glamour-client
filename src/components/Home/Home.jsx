7bc8477521e173397cb44a3113b31b854d2f1152
mim chowdhury <umac3333@gmail.com>
2024-08-16 02:10:53 +0600

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from '../Nav/Nav';
+import Footer from '../Footer/Footer';
+
+const Home = () => {
+    return (
+        <div >
+            <div className='container mx-auto'>
+                <Nav></Nav>
+            </div>
+            <Footer></Footer>
+        </div>
+
+    );
+};
+
+export default Home;
